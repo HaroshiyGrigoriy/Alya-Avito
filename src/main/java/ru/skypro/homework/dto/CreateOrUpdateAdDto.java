75fdb9877d4d2fa913bb0ffe71737560abe7fd39
HaroshiyGrigoriy <guruwolf8@yandex.ru>
2025-03-04 13:52:05 +0300

    Add validation for fields in newly created and updated entities

--- src/main/java/ru/skypro/homework/dto/CreateOrUpdateAdDto.java
+++ src/main/java/ru/skypro/homework/dto/CreateOrUpdateAdDto.java
@@ -1,13 +1,21 @@
 package ru.skypro.homework.dto;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
 public class CreateOrUpdateAdDto {
+    @NotBlank
+    @Size(min = 4, max = 32, message = "Введите название")
     private String title;
+    @Size(max = 10000000)
     private String price;
+    @NotBlank
+    @Size(min = 8, max = 64, message = "Описание обязательно!")
     private String description;
 }
